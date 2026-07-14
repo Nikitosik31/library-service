c399a0650b3289bc7a10460cd9f529d12479d995
Nik <nikita.pitushkin31@mail.com>
2026-07-14 16:47:20 +0300

    feat: add create borrowing with inventory decrement

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -17,3 +17,22 @@ class BorrowingSerializer(serializers.ModelSerializer):
             "book",
             "user",
         )
+
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+        )
+
+    def validate_book(self, value):
+        if value.inventory <= 0:
+            raise serializers.ValidationError(
+                "Inventory must be greater than 0"
+            )
+        return value
--- borrowing/views.py
+++ borrowing/views.py
@@ -1,9 +1,25 @@
+from django.db import transaction
 from rest_framework import viewsets
 
 from borrowing.models import Borrowing
-from borrowing.serializers import BorrowingSerializer
+from borrowing.serializers import (
+    BorrowingSerializer,
+    BorrowingCreateSerializer,
+)
 
 
-class BorrowViewSet(viewsets.ReadOnlyModelViewSet):
+class BorrowViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return BorrowingCreateSerializer
+        return BorrowingSerializer
+
+    def perform_create(self, serializer):
+        with transaction.atomic():
+            book = serializer.validated_data["book"]
+            book.inventory -= 1
+            book.save()
+            serializer.save(user=self.request.user)
