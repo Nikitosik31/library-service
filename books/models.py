72d13ba0583dc85fb91bbbac6f17aee62201512f
Nik <nikita.pitushkin31@mail.com>
2026-07-14 14:29:20 +0300

    feat: add book CRUD API

--- books/models.py
+++ books/models.py
@@ -12,3 +12,6 @@ class Book(models.Model):
     cover = models.CharField(max_length=4, choices=CoverType.choices)
     inventory = models.PositiveIntegerField()
     daily_fee = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.title
