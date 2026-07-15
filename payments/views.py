d258265b377bd58c46f4fccffb7396edcdaf2467
Nik <nikita.pitushkin31@mail.com>
2026-07-15 12:28:08 +0300

    feat: add payment model and list/detail endpoints

---
+++ payments/views.py
@@ -0,0 +1,17 @@
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from payments.models import Payment
+from payments.serializers import PaymentSerializer
+
+
+class PaymentViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = Payment.objects.all()
+        if not self.request.user.is_staff:
+            queryset = queryset.filter(borrowing__user=self.request.user)
+        return queryset
